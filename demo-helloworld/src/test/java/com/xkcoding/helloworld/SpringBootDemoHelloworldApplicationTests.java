929f4ca3de6e71c2dd48355bafd245ae14e9e5cb
tiieeiiwang <tiieeiiwang@tencent.com>
2022-04-07 17:03:02 +0800

    helloworld项目，单元测试controller中的方法

--- demo-helloworld/src/test/java/com/xkcoding/helloworld/SpringBootDemoHelloworldApplicationTests.java
+++ demo-helloworld/src/test/java/com/xkcoding/helloworld/SpringBootDemoHelloworldApplicationTests.java
@@ -2,6 +2,7 @@ package com.xkcoding.helloworld;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +10,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringBootDemoHelloworldApplicationTests {
 
+
+    // 单元测试controller中的方法
+    @Autowired
+    HelloWorldController helloWorldController;
+
     @Test
     public void contextLoads() {
+        String s = helloWorldController.hello("jack");
+        System.out.println(s);
     }
 
 }
